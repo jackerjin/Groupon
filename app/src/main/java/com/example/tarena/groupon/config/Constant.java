ebcd109fbaecc7abe52f5fc8b0845301577e14ea
jackerjin <956959794@qq.com>
2017-06-15 18:33:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tarena/groupon/config/Constant.java
@@ -0,0 +1,9 @@
+package com.example.tarena.groupon.config;
+
+/**
+ * Created by tarena on 2017/6/15.
+ */
+
+public class Constant {
+    public static final String FIRST="first";
+}
---
+++ app/src/main/java/com/example/tarena/groupon/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tarena.groupon.ui;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tarena.groupon.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
