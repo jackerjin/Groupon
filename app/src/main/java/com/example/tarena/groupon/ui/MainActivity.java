dd23164e14e81209d66ae37f89e647217510f032
jackerjin <956959794@qq.com>
2017-06-16 10:14:31 +0800

    下来刷新

--- app/src/main/java/com/example/tarena/groupon/ui/MainActivity.java
+++ app/src/main/java/com/example/tarena/groupon/ui/MainActivity.java
@@ -1,16 +1,73 @@
 package com.example.tarena.groupon.ui;
 
 import android.app.Activity;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.tarena.groupon.R;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+    @BindView(R.id.pullToRefresh_ListView_main)
+    PullToRefreshListView ptrListView;
+    ListView listView;
+    List<String> datas;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initListView();
+    }
+
+    private void initListView() {
+        listView = ptrListView.getRefreshableView();
+        datas = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,datas);
+        listView.setAdapter(adapter);
+        //添加下来松手后的刷新
+        ptrListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        datas.add(0, "新增内容");
+                        adapter.notifyDataSetChanged();
+                        ptrListView.onRefreshComplete();
+                    }
+                }, 1500);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refresh();
+    }
+
+    private void refresh() {
+        datas.add("aaa");
+        datas.add("bbb");
+        datas.add("ccc");
+        datas.add("ddd");
+        datas.add("eee");
+        datas.add("fff");
+        datas.add("ggg");
+        adapter.notifyDataSetChanged();
+
     }
 }
